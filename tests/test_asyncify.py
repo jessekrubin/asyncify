0e7b983b6ab368d0e5d3c499dcc4ead726531c43
jessekrubin <jessekrubin@gmail.com>
2019-10-02 22:17:17 -0700

    YEAH:

--- tests/test_asyncify.py
+++ tests/test_asyncify.py
@@ -1,1 +1,28 @@
+import asyncio
 from asyncify import asyncify
+import things
+import pytest
+print(dir(things))
+asyncify(things)
+
+print(dir(things))
+
+def fsync(a, b):
+    r = a + b
+    return r
+
+fasync = asyncify(fsync)
+
+@pytest.mark.asyncio
+async def test_asyncify_module():
+    rasync = await things.add_async(1, 2)
+    rsync = things.add(1, 2)
+    assert rsync == rasync
+
+@pytest.mark.asyncio
+async def test_asyncify_function():
+    rsync = fsync(1, 4)
+    rasync = await fasync(1, 4)
+    assert rsync == rasync
+
+
