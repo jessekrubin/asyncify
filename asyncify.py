c3aadaa9636fe3b60508597e15798cc5f41ef72e
jessekrubin <jessekrubin@gmail.com>
2019-11-06 19:51:26 -0800

    asyncify works with classes

--- asyncify.py
+++ asyncify.py
@@ -7,7 +7,7 @@ from asyncio import coroutine
 from asyncio import get_event_loop
 from functools import partial
 from functools import wraps
-from inspect import ismodule, getmembers, isfunction
+from inspect import ismodule, getmembers, isfunction, isclass, ismethod
 from typing import Any
 from typing import Callable
 from typing import TypeVar
@@ -16,7 +16,7 @@ FuncType = Callable[..., Any]
 F = TypeVar("F", bound=FuncType)
 
 
-def asyncify(funk: F) -> F:
+def asyncify(funk: F, functions=True, classes = True) -> F:
     """
 
     :param funk:
@@ -37,10 +37,28 @@ def asyncify(funk: F) -> F:
         loop = loop if loop else get_event_loop()
         pfunc = partial(funk, *args, **kwargs)
         return loop.run_in_executor(executor, pfunc)
-
+    print(funk)
     if ismodule(funk):
         for fname, f in getmembers(funk, isfunction):
             setattr(funk, '{}_async'.format(fname), asyncify(f))
+        for fname, f in getmembers(funk, isclass):
+            setattr(funk, '{}_async'.format(fname), asyncify(f))
+    elif isclass(funk):
+        print(funk)
+        for a, b in funk.__dict__.items():
+            print(a, b)
+        print(getmembers(funk, predicate=ismethod))
+        # print(getmembers(funk))
+        members = [el for el in getmembers(funk) if not el[0].startswith('__') and not el[0].endswith('__')]
+        print(members)
+
+        for mem in members:
+            setattr(funk, '{}_async'.format(mem[0]), asyncify(mem[-1]))
+        members = [el for el in getmembers(funk) if not el[0].startswith('__') and not el[0].endswith('__')]
+
+        print(members)
+
+        # setattr(funk, '{}_async'.format(fname), asyncify(f))
     else:
         return cast(F, afunk)
 
